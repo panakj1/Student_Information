04c970ff0e881931185b5f19ebf2e81245fdff34
Pankaj <pankajbhardwajtyagi194@gmail.com>
2023-12-27 11:59:16 +0530

    initial commit

---
+++ src/test/java/com/StudentInformaiton/StudentInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentInformaiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
