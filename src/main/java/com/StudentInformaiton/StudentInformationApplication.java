5bdb1edc748e1376ecf8dc1d053fb3dba1effc08
Pankaj <pankajbhardwajtyagi194@gmail.com>
2023-12-30 15:18:31 +0530

    Commit S

--- src/main/java/com/StudentInformaiton/StudentInformationApplication.java
+++ src/main/java/com/StudentInformaiton/StudentInformationApplication.java
@@ -8,6 +8,7 @@ public class StudentInformationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentInformationApplication.class, args);
+		System.out.println("---------");
 	}
 
 }
